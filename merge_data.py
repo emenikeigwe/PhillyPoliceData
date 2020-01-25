2aa477575df74449ce7f6f737e23f4e2ada34763
“Amelia” <ameliadogan@gmail.com>
2020-01-25 12:33:24 -0500

    intermitent push

--- merge_data.py
+++ merge_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import re, datetime
 
 csv1= 'policedata/ppd_complaints.csv'
 csv2 = 'policedata/ppd_complainant_demographics.csv'
@@ -17,11 +18,28 @@ df_dem = pd.read_csv(csv3)
 df_merge1 = df_complaints.merge(df_outcome)
 #combine three csvs
 df_all_data = df_merge1.merge(df_dem)
-print(df_all_data)
+#print(df_all_data)
 
 ##parse data to get actual date
 
 dates = []
-for time in df_all_data['summary']:
-    print(time.split("at")[0])
-print(dates)
+date_format = r"(\d{1,2}-\d{1,2}-\d{2,4})"
+df_all_data['complaint_date'] = df_all_data['summary'].str.extract(date_format)
+
+test_string = "sdsfsd"
+# get place
+indexNames = df_all_data[df_all_data['complaint_date'].isnull()].index
+df_all_data.drop(indexNames, inplace=True)
+print(df_all_data.iloc[90]['complaint_date'])
+df_all_data['complaint_date'] = pd.to_datetime(df_all_data['complaint_date'], errors="ignore")
+test_type = type(df_all_data.iloc[81]['complaint_date'])
+print(df_all_data['complaint_date'][type(df_all_data['complaint_date']) == type(test_string)].index)
+
+#print(df_all_data[type(df_all_data['complaint_date']) == type(test_string)].index)
+# print(df_all_data.iloc[5491]['summary'])
+
+# for r in df_all_data['complaint_date']:
+#     print(type(r))
+
+#df_all_data.to_csv('cleaned_police_data.csv')
+# 
